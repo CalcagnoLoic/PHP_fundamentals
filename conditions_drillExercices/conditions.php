4bb9454635090b31e5a59540a040aef5f6e2fd9a
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-04-11 19:15:43 +0200

    ajout des exercices sur les conditions

--- conditions_drillExercices/conditions.php
+++ conditions_drillExercices/conditions.php
@@ -1,13 +1,141 @@
+<h2>Exercice n°1 : Clean your room</h2>
 <?php
-/**
- * ==================================================
- * Séries d'exercices sur les conditions en PHP
- * ==================================================
- */
-
 /**
   *-----------------------------------
   *         Exercice n°1
+  *       Clean your room!
   *-----------------------------------
   */
+$possible_states = [
+    0 => "health hazard",
+    1 => "filthy", 
+    2 => "dirty", 
+    3 => "clean",
+    4 => "immaculate"
+];
+
+$room_filthiness = $possible_states[2];
+
+if($room_filthiness === "health hazard") {
+    echo "Room is digusting! Let's clean it up!"; 
+} else if ($room_filthiness == "filthy") {
+    echo "Room is filthy : let's clean it up!";
+} else if ($room_filthiness == "dirty") {
+    echo "Room is dirty : let's clean it up!";
+} else if ($room_filthiness == "clean") {
+    echo "Room is clean, well done!";
+} else {
+    echo "Nothing to do, room is neat.";
+}
+?> 
+
+<h2>Exercice n°2 : Greeting message according to hour</h2>
+<?php 
+/**
+  *-----------------------------------------------------------------------------
+  *                              Exercice n°2
+  *     Display a different greeting message depending on the time of the day
+  *-----------------------------------------------------------------------------
+  */
+$now = date('H:i');
+
+if($now>='05:00' && $now<='09:00') {echo "Good morning!" ;} 
+if($now > '09:01' && $now <= '12:00') {echo "Good day!" ;} 
+if($now > '12:01' && $now <= '16:00') {echo "Good afternoon!";}
+if($now > '16:01' && $now <='21:00') {echo "Good evening!";} 
+if($now > '21:01' && $now <= '04:59') {echo "Good night!";};
+?>
+
+<h2>Exercice n°3/4/5 : Greeting message according to the user's age (3), the gender (4) and the mothertongue (5)</h2>
+
+<form method="get" action="">
+	<label for="age">Please type your age  : </label>
+	<input type="" name="age"><br>
+    <label for="radio">Man or Woman?  : </label>
+    <input type="radio" name="man"> Man
+    <input type="radio" name="woman"> Woman <br>
+    <label for="gender">Do you speak Engligh?  : </label>
+    <input type="radio" name="man"> yes
+    <input type="radio" name="woman"> no <br>
+	<input type="submit" name="submit" value="Greet me now">
+</form>
+
+<?php
+/**
+  *-------------------------------------------------------------------------------------------------
+    *                                     Exercice n°3
+    *               Display a different greeting message depending on the age of user
+  *-------------------------------------------------------------------------------------------------
+  *-------------------------------------------------------------------------------------------------
+  *                                         Exercice n°4
+  *             Display a different greeting message depending on the age of user and gender
+  *-------------------------------------------------------------------------------------------------
+  *-------------------------------------------------------------------------------------------------
+  *                                         Exercice n°5
+  *     Display a different greeting message depending on the age of user, gender and mothertongue
+  *-------------------------------------------------------------------------------------------------
+  */
+
+if(isset($_GET['age'])){
+    if($_GET['age'] <= 12) {echo "Hello kiddo!";} 
+    else if ($_GET['age'] <= 18){echo "Hello Teenager!";} 
+    else if ($_GET['age'] <= 115) {echo "Hello Adult!";} 
+    else {echo "Wow! Still alive? Are you a robot, like me? Can I hug you?";}  
+}    
+?>
+
+<h2>Exercice n°6 : The girl soccer team</h2>
+
+<form action="" method="get">
+    <label for="age">Please, type your age : </label>
+    <input type="" name="age"><br>
+    <label for="name">Please, type your name : </label>
+    <input type="" name="name"><br>
+    <label for="gender"> Please, type your gender (enter woman or man <strong>ONLY</strong>) : </label>
+    <input type="" name="gender"><br>
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+/**
+  *----------------------------------------------------
+  *                 Exercice n°6
+  *       Display a message according to info in input
+  *----------------------------------------------------
+  */
+
+if(isset($_GET['age']) AND isset($_GET['gender'])){
+    if($_GET['gender'] == "woman" && $_GET['age'] > 21 && $_GET['age'] < 40) {
+        echo "welcome to the team !";
+    } else {
+        echo "Sorry you don't fit the criteria";
+    }
+}
+?>
+
+
+<h2>Exercice n°8 : School sucks</h2>
+
+<form action="" method="get">
+    <label for="note">Your notation is : </label>
+    <input type="" name="note">
+    <input type="submit" value="Submit" name="submit">
+</form>
+
+<?php
+/**
+  *----------------------------------------------------
+  *                 Exercice n°8
+  *       Display a grade according to the note
+  *----------------------------------------------------
+  */
+
+if(isset($_GET['note'])){
+    if($_GET['note'] < 4){echo "This work is really bad. What a dumb kid!";}
+    if($_GET['note'] <=9 && $_GET['note']> 4) {echo "This is not sufficient. More studying is required.";}
+    if($_GET['note'] == 10) {echo "barely enough!";}
+    if($_GET['note'] <=14 && $_GET['note']> 10) {echo "Not bad!";}
+    if($_GET['note'] <=18 && $_GET['note']> 14) {echo "Bravo, bravissimo!";}
+    if($_GET['note'] == 20 && $_GET['note']> 18) {echo "Too good to be true : confront the cheater!";}
+}
 ?>
